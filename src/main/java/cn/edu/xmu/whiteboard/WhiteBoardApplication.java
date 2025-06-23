27198a1a33226f6acd875922158c992eac4ecf30
Huang ZhenSong <2740988813@qq.com>
2025-06-23 16:41:47 +0800

    sql文件

--- src/main/java/cn/edu/xmu/whiteboard/WhiteBoardApplication.java
+++ src/main/java/cn/edu/xmu/whiteboard/WhiteBoardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WhiteBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhiteBoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteBoardApplication.class, args);
+    }
 
 }
