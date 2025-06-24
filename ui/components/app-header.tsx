c29a3f98c84bc0654efaa85fb2d5c1a56cc9ece4
llmons <llmons@foxmail.com>
2025-06-24 20:19:31 +0800

    feat: add environment variable support and improve UI components
    - Updated `.gitignore` to explicitly include `.env` files and clarified comments.
    - Replaced hardcoded `BASE_URL` with `NEXT_PUBLIC_BASE_URL` environment variable in `constants.ts`.
    - Added instructions for configuring `.env` in the README.
    - Enhanced `AppHeader` with logout functionality and improved button styles.
    - Updated `ModeToggle` to include `cursor-pointer` for better UX.

--- ui/components/app-header.tsx
+++ ui/components/app-header.tsx
@@ -2,10 +2,13 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { useUserStore } from "@/stores/user";
 import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 import * as React from "react";
 
 export default function AppHeader() {
+  const router = useRouter();
   const username = useUserStore((state) => state.username);
+  const logout = useUserStore((state) => state.logout);
 
   return (
     <header className="bg-background sticky top-0 flex items-center gap-2 px-2 py-1.5 md:px-2">
@@ -15,7 +18,15 @@ export default function AppHeader() {
       <div className="order-4 hidden h-fit items-center gap-2 px-2 py-1.5 md:ml-auto md:flex md:h-[34px]">
         <h4 className="text-sm leading-none font-medium">{username}</h4>
         <ModeToggle />
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className="cursor-pointer"
+          onClick={async () => {
+            await logout();
+            router.replace("/login");
+          }}
+        >
           <LogOut className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
         </Button>
       </div>
--- ui/lib/constants.ts
+++ ui/lib/constants.ts
@@ -1,7 +1,5 @@
-const BASE_URL = "http://127.0.0.1:4523/m1/6629948-6337524-default";
-
 export const ENDPOINT = {
-  Register: `${BASE_URL}/api/register`,
-  Login: `${BASE_URL}/api/login`,
-  Logout: `${BASE_URL}/api/logout`,
+  Register: `${process.env.NEXT_PUBLIC_BASE_URL}/api/register`,
+  Login: `${process.env.NEXT_PUBLIC_BASE_URL}/api/login`,
+  Logout: `${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`,
 };
