c9e3859c7550d24c5be0ed69cd08a6ff3653edb1
Huang ZhenSong <2740988813@qq.com>
2025-06-25 16:30:35 +0800

    新建画板

---
+++ src/main/java/cn/edu/xmu/whiteboard/ReturnData/DrawBoardReturnData.java
@@ -0,0 +1,14 @@
+package cn.edu.xmu.whiteboard.ReturnData;
+
+public class DrawBoardReturnData {
+    private String id;
+    public DrawBoardReturnData(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/cn/edu/xmu/whiteboard/redis/DrawBoardKey.java
@@ -0,0 +1,15 @@
+package cn.edu.xmu.whiteboard.redis;
+
+
+public class DrawBoardKey extends BasePrefix{
+
+    private DrawBoardKey(String prefix) {
+        super(prefix);
+    }
+    private DrawBoardKey(int expireSeconds,String prefix) {
+        super(expireSeconds,prefix);
+    }
+
+    public static DrawBoardKey getById = new DrawBoardKey(0, "db");
+    public static DrawBoardKey getByHash = new DrawBoardKey("db:");
+}
