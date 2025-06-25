31d58529e53049dacb5d251e927b16302fffe908
Huang ZhenSong <2740988813@qq.com>
2025-06-25 14:55:51 +0800

    连接前端

--- src/main/java/cn/edu/xmu/whiteboard/controller/ProjectController.java
+++ src/main/java/cn/edu/xmu/whiteboard/controller/ProjectController.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api", produces = "application/json;charset=UTF-8")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProjectController {
 
     @Autowired
