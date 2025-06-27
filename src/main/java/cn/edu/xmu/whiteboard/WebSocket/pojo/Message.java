798dc3e25daca9e5b6014e539127340d4cb35f29
康艺瀚 <1509985712@qq.com>
2025-06-27 21:31:07 +0800

    webSocket发送信息

---
+++ src/main/java/cn/edu/xmu/whiteboard/WebSocket/pojo/Message.java
@@ -0,0 +1,36 @@
+package cn.edu.xmu.whiteboard.WebSocket.pojo;
+
+public class Message {
+    private String name;
+    private String message;
+
+    public Message() {
+
+    }
+
+    public Message(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String toJsonString(){
+        return "{\"name\":\" "+name+"\"," +"\"message\":\" "+message+"\"}";
+    }
+}
