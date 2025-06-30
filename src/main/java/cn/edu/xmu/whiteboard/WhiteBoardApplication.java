a889d07e768ac8096ee1acc8efc7625dd029184c
康艺瀚 <1509985712@qq.com>
2025-06-30 11:30:20 +0800

    改回websocket

--- src/main/java/cn/edu/xmu/whiteboard/WhiteBoardApplication.java
+++ src/main/java/cn/edu/xmu/whiteboard/WhiteBoardApplication.java
@@ -1,26 +1,13 @@
 package cn.edu.xmu.whiteboard;
 
-import com.corundumstudio.socketio.SocketIOServer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WhiteBoardApplication implements CommandLineRunner{
-    @Autowired
-    private SocketIOServer socketIOServer;
+public class WhiteBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WhiteBoardApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-        socketIOServer.start(); // 启动 Socket.IO 服务器
-        System.out.println("Socket.IO server started on port 8081");
-        socketIOServer.addConnectListener(client -> {
-            System.out.println("Client connected: " + client.getSessionId());
-        });
-    }
 }
