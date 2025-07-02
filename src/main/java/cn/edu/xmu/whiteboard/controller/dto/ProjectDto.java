1896a4d1eb8d7678fb769ee15ba898fa4a358cf3
Huang ZhenSong <2740988813@qq.com>
2025-07-02 09:16:56 +0800

    新建项目增加模板参数

--- src/main/java/cn/edu/xmu/whiteboard/controller/dto/ProjectDto.java
+++ src/main/java/cn/edu/xmu/whiteboard/controller/dto/ProjectDto.java
@@ -3,10 +3,14 @@ package cn.edu.xmu.whiteboard.controller.dto;
 public class ProjectDto {
     private String name;
     private String description;
+    private String template;
 
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
 
     public String getDescription() {return description;}
     public void setDescription(String description) {this.description = description;}
+
+    public String getTemplate() {return template;}
+    public void setTemplate(String template) {this.template = template;}
 }
