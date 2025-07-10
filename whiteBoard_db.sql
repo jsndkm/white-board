d83daf2c709cf815bc841b43e9499ce7503c6ade
Huang ZhenSong <2740988813@qq.com>
2025-07-10 10:19:31 +0800

    删除idea表

--- whiteBoard_db.sql
+++ whiteBoard_db.sql
@@ -29,22 +29,6 @@ CREATE TABLE `project` (
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for idea
--- ----------------------------
-DROP TABLE IF EXISTS `idea`;
-CREATE TABLE `idea` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` text,
-  `username` varchar(255) NOT NULL,
-  `project_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_user` (`username`),
-  KEY `fk_project` (`project_id`),
-  CONSTRAINT `fk_idea_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_idea_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
-
--- ----------------------------
 -- Table structure for project_member
 -- ----------------------------
 DROP TABLE IF EXISTS `project_user`;
